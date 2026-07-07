a16e028b847c82d4732adc889a7953d8b3c424e3
Félix Laplante <75139846+felixlaplante0@users.noreply.github.com>
2026-07-07 15:00:45 +0200

    Rename do base

--- sbcluster/__init__.py
+++ sbcluster/__init__.py
@@ -1,6 +1,6 @@
 """Spectral Bridges clustering and dimension reduction algorithm."""
 
-from ._cluster import SpectralBridges
+from ._base import SpectralBridges
 from ._score import ngap_scorer, silhouette_scorer
 
 __all__ = [
